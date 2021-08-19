fb88aa6cc4eee6ed1c1501cdbbe1f940d8c15c9e
echang1 <echang1@ftdm.com.tw>
2021-08-19 23:50:44 +0800

    first commit

---
+++ member-service/src/test/java/com/club/memberservice/MemberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.club.memberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
